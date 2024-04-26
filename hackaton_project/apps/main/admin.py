93b2f754dab2db783569d7cb870d43378f45bf22
Revy1664 <steinsgate95@mail.ru>
2024-04-27 03:29:10 +0600

    final editing

--- hackaton_project/apps/main/admin.py
+++ hackaton_project/apps/main/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 from .models import QuestionAnswer
 
-
-admin.site.register(QuestionAnswer)
+# registr model in admin panel
+admin.site.register(QuestionAnswer) 
--- hackaton_project/apps/main/forms.py
+++ hackaton_project/apps/main/forms.py
@@ -4,11 +4,17 @@ from .models import QuestionAnswer
 
 
 class QuestionAnswerForm(forms.ModelForm):
-
+	"""
+		Form for creating Question
+	"""
 	class Meta:
 		model = QuestionAnswer
 		fields = ["question"]
 
 
 class SubjectForm(forms.Form):
+	"""
+		Form for specify subject
+	"""
+
 	subject = forms.CharField(max_length=30)
