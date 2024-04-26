e3499411013c9858d54b086bd96da8114084fa56
Revy1664 <steinsgate95@mail.ru>
2024-04-27 00:32:10 +0600

    made ask-quesion stage

---
+++ hackaton_project/apps/main/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import QuestionAnswer
+
+
+class QuestionAnswerForm(forms.ModelForm):
+
+	class Meta:
+		model = QuestionAnswer
+		fields = ["question"]
--- hackaton_project/apps/main/urls.py
+++ hackaton_project/apps/main/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("ask_question", views.ask_question, name="ask_question"),
+    path("questions", views.all_questions, name="questions"),
 ]
